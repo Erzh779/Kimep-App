5b7957ccf62cd0bfad5e1c635538f93c8c1ebb29
Erzh779 <erzhan7taipov@mail.ru>
2021-11-30 17:53:12 +0600

    Initial Commit

---
+++ Kimep App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kimep App
+//
+//  Created by Erzhan Taipov on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
